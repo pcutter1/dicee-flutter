732349de9238264747e6f7caddfeaff7b9735637
Paul C <pcutter@cnm.edu>
2020-08-18 17:50:38 -0600

    Began creating rng method

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 1;
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+                onPressed: () {
+                  print('Right button got pressed.');
+              },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
